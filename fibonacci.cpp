8142be01adadac5f8406be9b3a7b7dce2b20d6a1
Rupender-Bhati <bhati730@gmail.com>
2020-08-19 12:21:28 +0530

    First Code

---
+++ fibonacci.cpp
@@ -0,0 +1,18 @@
+
+#include<iostream>
+
+class Solution {
+public:
+    int fib(int N) {
+        
+        if(N<2)
+        {
+            return  N;
+        }
+        
+        else
+            return fib(N-1)+fib(N-2);
+        
+        
+    }
+};
